ac188fde12cd5267a5591420199ca4ce96546cec
verenazmo <verena.zmollnig@htl-salzburg.ac.at>
2021-04-10 22:46:32 +0200

    delete unnecessary lines

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -2,17 +2,15 @@
 
 public class Obstacle : MonoBehaviour
 {
-
     Rigidbody2D rb;
     [SerializeField] private float MoveSpeed;
 
     private void Awake()
     {
-    rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-
-// Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
         //if obstacle's position x is < -15f it will be destroyed
@@ -20,15 +18,12 @@ public class Obstacle : MonoBehaviour
         {
             Destroy(gameObject);
         }
-            //if obstacle's position x is < -15f it will be destroyed
-            if (transform.position.x > 15f)
-            {
-                Destroy(gameObject);
-            }
-
+        //if obstacle's position x is < -15f it will be destroyed
+        if (transform.position.x > 15f)
+        {
+            Destroy(gameObject);
+        }
     }
-
-
 
     private void FixedUpdate()
     {
